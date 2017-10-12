be2732791e64de752467febe26b30f8ec74e43b7
anandp219 <anpandey@iitk.ac.in>
2017-10-12 17:22:36 +0530

    Url Check added

--- utility.go
+++ utility.go
@@ -8,6 +8,7 @@ const (
 	ASCII_VALUE_0 = 48
 	ASCII_VALUE_9 = 57
 	EMAIL_REGEX = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+	URL_REGEX = "^((((H|h)(T|t)|(F|f))(T|t)(P|p)((S|s)?))\\://)?(www.|[a-zA-Z0-9].)[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,6}(\\:[0-9]{1,5})*(/($|[a-zA-Z0-9\\.\\,\\;\\?\\'\\\\+&amp;%\\$#\\=~_\\-]+))*$"
 )
 
 func IsNumber (str string) bool {
@@ -23,13 +24,11 @@ func IsNumber (str string) bool {
 }
 
 func IsEmail (str string) bool {
-	if len(str) == 0 {
-		return false
-	}
 	var validEmail = regexp.MustCompile(EMAIL_REGEX)
 	return validEmail.MatchString(str)
 }
 
-
-
-
+func IsUrl (str string) bool {
+	var validEmail = regexp.MustCompile(URL_REGEX)
+	return validEmail.MatchString(str)
+}
