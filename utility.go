511603cca3eddb6f01f4031e07ba7edf5fe25d0b
anandp219 <anpandey@iitk.ac.in>
2017-10-13 20:25:24 +0530

    gofmt command was run

--- utility.go
+++ utility.go
@@ -8,11 +8,11 @@ import (
 const (
 	ASCII_VALUE_0 = 48
 	ASCII_VALUE_9 = 57
-	EMAIL_REGEX = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
-	URL_REGEX = "^((((H|h)(T|t)|(F|f))(T|t)(P|p)((S|s)?))\\://)?(www.|[a-zA-Z0-9].)[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,6}(\\:[0-9]{1,5})*(/($|[a-zA-Z0-9\\.\\,\\;\\?\\'\\\\+&amp;%\\$#\\=~_\\-]+))*$"
+	EMAIL_REGEX   = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+	URL_REGEX     = "^((((H|h)(T|t)|(F|f))(T|t)(P|p)((S|s)?))\\://)?(www.|[a-zA-Z0-9].)[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,6}(\\:[0-9]{1,5})*(/($|[a-zA-Z0-9\\.\\,\\;\\?\\'\\\\+&amp;%\\$#\\=~_\\-]+))*$"
 )
 
-func IsNumber (str string) bool {
+func IsNumber(str string) bool {
 	if len(str) == 0 {
 		return false
 	}
@@ -24,41 +24,41 @@ func IsNumber (str string) bool {
 	return true
 }
 
-func IsEmail (str string) bool {
+func IsEmail(str string) bool {
 	var validEmail = regexp.MustCompile(EMAIL_REGEX)
 	return validEmail.MatchString(str)
 }
 
-func IsUrl (str string) bool {
+func IsUrl(str string) bool {
 	var validEmail = regexp.MustCompile(URL_REGEX)
 	return validEmail.MatchString(str)
 }
 
-func IsTitleCase (str string) bool {
+func IsTitleCase(str string) bool {
 	titleCase := strings.Title(str)
 	result := strings.Compare(str, titleCase)
 	return result == 0
 }
 
-func FlattenFloat64 (matrix [][]float64) []float64 {
+func FlattenFloat64(matrix [][]float64) []float64 {
 	row := []float64{}
-	for _,value := range matrix {
+	for _, value := range matrix {
 		row = append(row, value...)
 	}
 	return row
 }
 
-func FlattenInt (matrix [][]int) []int {
+func FlattenInt(matrix [][]int) []int {
 	row := []int{}
-	for _,value := range matrix {
+	for _, value := range matrix {
 		row = append(row, value...)
 	}
 	return row
 }
 
-func FlattenString (matrix [][]string) []string {
+func FlattenString(matrix [][]string) []string {
 	row := []string{}
-	for _,value := range matrix {
+	for _, value := range matrix {
 		row = append(row, value...)
 	}
 	return row
