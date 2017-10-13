61b7dc5e47541ae90f07b696af6e162a5f2032de
anandp219 <anpandey@iitk.ac.in>
2017-10-14 01:57:57 +0530

    Row to matrix conversion added. IsNumber using regex for number checking

--- utility.go
+++ utility.go
@@ -3,25 +3,19 @@ package gotility
 import (
 	"regexp"
 	"strings"
+	"errors"
+	"fmt"
 )
 
 const (
-	ASCII_VALUE_0 = 48
-	ASCII_VALUE_9 = 57
+	NUMBER_REGEX  = "^[+-]?([0-9]+(\\.[0-9]*)?|\\.[0-9]+)([eE][+-]?[0-9]+)?$"
 	EMAIL_REGEX   = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
 	URL_REGEX     = "^((((H|h)(T|t)|(F|f))(T|t)(P|p)((S|s)?))\\://)?(www.|[a-zA-Z0-9].)[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,6}(\\:[0-9]{1,5})*(/($|[a-zA-Z0-9\\.\\,\\;\\?\\'\\\\+&amp;%\\$#\\=~_\\-]+))*$"
 )
 
 func IsNumber(str string) bool {
-	if len(str) == 0 {
-		return false
-	}
-	for index := range str {
-		if uint8(str[index]) < ASCII_VALUE_0 || uint8(str[index]) > ASCII_VALUE_9 {
-			return false
-		}
-	}
-	return true
+	var validNumber = regexp.MustCompile(NUMBER_REGEX)
+	return validNumber.MatchString(str)
 }
 
 func IsEmail(str string) bool {
@@ -62,4 +56,46 @@ func FlattenString(matrix [][]string) []string {
 		row = append(row, value...)
 	}
 	return row
+}
+
+func ToMatrixInt(row []int, numRows int) ([][]int, error) {
+	length := len(row)
+	if numRows <= 0 || length % numRows != 0 {
+		return nil,errors.New(fmt.Sprintf("divison of row to matrix not possible. Invalid numRows : %d ", numRows))
+	}
+	numCols := length/numRows
+	matrix := make([][]int, numRows)
+	for i := 0 ; i < numRows ; i += 1 {
+		index := i*numCols
+		matrix[i] = append(matrix[i], row[index: index+numCols]...)
+	}
+	return matrix, nil
+}
+
+func ToMatrixString(row []string, numRows int) ([][]string, error) {
+	length := len(row)
+	if numRows <= 0 || length%numRows != 0 {
+		return nil, errors.New(fmt.Sprintf("divison of row to matrix not possible. Invalid numRows : %d ", numRows))
+	}
+	numCols := length / numRows
+	matrix := make([][]string, numRows)
+	for i := 0; i < numRows; i += 1 {
+		index := i * numCols
+		matrix[i] = append(matrix[i], row[index: index+numCols]...)
+	}
+	return matrix, nil
+}
+
+func ToMatrixFloat64(row []float64, numRows int) ([][]float64, error) {
+	length := len(row)
+	if numRows <= 0 || length%numRows != 0 {
+		return nil, errors.New(fmt.Sprintf("divison of row to matrix not possible. Invalid numRows : %d ", numRows))
+	}
+	numCols := length / numRows
+	matrix := make([][]float64, numRows)
+	for i := 0; i < numRows; i += 1 {
+		index := i * numCols
+		matrix[i] = append(matrix[i], row[index: index+numCols]...)
+	}
+	return matrix, nil
 }
