3dd252bc32d41ca97c99cb5528d7eacbbfb23c2d
anandp219 <anpandey@iitk.ac.in>
2017-10-14 02:00:43 +0530

    gofmt run

--- utility.go
+++ utility.go
@@ -1,16 +1,16 @@
 package gotility
 
 import (
-	"regexp"
-	"strings"
 	"errors"
 	"fmt"
+	"regexp"
+	"strings"
 )
 
 const (
-	NUMBER_REGEX  = "^[+-]?([0-9]+(\\.[0-9]*)?|\\.[0-9]+)([eE][+-]?[0-9]+)?$"
-	EMAIL_REGEX   = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
-	URL_REGEX     = "^((((H|h)(T|t)|(F|f))(T|t)(P|p)((S|s)?))\\://)?(www.|[a-zA-Z0-9].)[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,6}(\\:[0-9]{1,5})*(/($|[a-zA-Z0-9\\.\\,\\;\\?\\'\\\\+&amp;%\\$#\\=~_\\-]+))*$"
+	NUMBER_REGEX = "^[+-]?([0-9]+(\\.[0-9]*)?|\\.[0-9]+)([eE][+-]?[0-9]+)?$"
+	EMAIL_REGEX  = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+	URL_REGEX    = "^((((H|h)(T|t)|(F|f))(T|t)(P|p)((S|s)?))\\://)?(www.|[a-zA-Z0-9].)[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,6}(\\:[0-9]{1,5})*(/($|[a-zA-Z0-9\\.\\,\\;\\?\\'\\\\+&amp;%\\$#\\=~_\\-]+))*$"
 )
 
 func IsNumber(str string) bool {
@@ -60,14 +60,14 @@ func FlattenString(matrix [][]string) []string {
 
 func ToMatrixInt(row []int, numRows int) ([][]int, error) {
 	length := len(row)
-	if numRows <= 0 || length % numRows != 0 {
-		return nil,errors.New(fmt.Sprintf("divison of row to matrix not possible. Invalid numRows : %d ", numRows))
+	if numRows <= 0 || length%numRows != 0 {
+		return nil, errors.New(fmt.Sprintf("divison of row to matrix not possible. Invalid numRows : %d ", numRows))
 	}
-	numCols := length/numRows
+	numCols := length / numRows
 	matrix := make([][]int, numRows)
-	for i := 0 ; i < numRows ; i += 1 {
-		index := i*numCols
-		matrix[i] = append(matrix[i], row[index: index+numCols]...)
+	for i := 0; i < numRows; i += 1 {
+		index := i * numCols
+		matrix[i] = append(matrix[i], row[index:index+numCols]...)
 	}
 	return matrix, nil
 }
@@ -81,7 +81,7 @@ func ToMatrixString(row []string, numRows int) ([][]string, error) {
 	matrix := make([][]string, numRows)
 	for i := 0; i < numRows; i += 1 {
 		index := i * numCols
-		matrix[i] = append(matrix[i], row[index: index+numCols]...)
+		matrix[i] = append(matrix[i], row[index:index+numCols]...)
 	}
 	return matrix, nil
 }
@@ -95,7 +95,7 @@ func ToMatrixFloat64(row []float64, numRows int) ([][]float64, error) {
 	matrix := make([][]float64, numRows)
 	for i := 0; i < numRows; i += 1 {
 		index := i * numCols
-		matrix[i] = append(matrix[i], row[index: index+numCols]...)
+		matrix[i] = append(matrix[i], row[index:index+numCols]...)
 	}
 	return matrix, nil
 }
