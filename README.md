63a46e0d6bc989adb13c083679c9854accbb4052
anandp219 <anpandey@iitk.ac.in>
2017-10-16 17:12:29 +0530

    some test cases added. Code coverage badge

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 [![Build Status](https://travis-ci.org/anandp219/gotility.svg?branch=master)](https://travis-ci.org/anandp219/gotility)
 [![Go Report Card](https://goreportcard.com/badge/github.com/anandp219/gotility)](https://goreportcard.com/report/github.com/anandp219/gotility)
 [![GoDoc](https://godoc.org/github.com/anandp219/gotility?status.svg)](https://godoc.org/github.com/anandp219/gotility)
+[![codecov](https://codecov.io/gh/anandp219/gotility/branch/master/graph/badge.svg)](https://codecov.io/gh/anandp219/gotility)
 
 A utility library for go
 
--- utility_test.go
+++ utility_test.go
@@ -2,6 +2,7 @@ package gotility
 
 import (
 	"testing"
+	"reflect"
 )
 
 func TestIsEmail(t *testing.T) {
@@ -45,5 +46,26 @@ func TestIsTitleCase(t *testing.T) {
 	isTitleCase = IsTitleCase("gO IS NOT POPULAR")
 	if isTitleCase != false {
 		t.Error("Expected false, got ", isTitleCase)
+	}
+}
+
+func TestFlattenFloat64(t *testing.T) {
+	row := FlattenFloat64([][]float64{{1.1, 2.2}, {3.3, 4.4}})
+	if !reflect.DeepEqual(row, []float64{1.1, 2.2, 3.3, 4.4}) {
+		t.Error("Expected [1.1 2.2 3.3 4.4], got ", row)
+	}
+}
+
+func TestFlattenInt(t *testing.T) {
+	row := FlattenInt([][]int{{1, 2}, {3, 4}})
+	if !reflect.DeepEqual(row, []int{1, 2, 3, 4}) {
+		t.Error("Expected [1 2 3 4], got ", row)
+	}
+}
+
+func TestFlattenString(t *testing.T) {
+	row := FlattenString([][]string{{"A", "B"}, {"C", "D"}})
+	if !reflect.DeepEqual(row, []string{"A", "B", "C", "D"}) {
+		t.Error("Expected [A B C D], got ", row)
 	}
 }
